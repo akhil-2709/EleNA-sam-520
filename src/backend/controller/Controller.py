c7c6e11f44291002fa085051ca99fbf3e69c83eb
akhil-2709 <aanthireddy@umass.edu>
2022-11-30 10:11:46 -0500

    Added Controller Interface and AstarController

---
+++ src/backend/controller/Controller.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class Controller(ABC):
+
+    def __init__(self):
+        self.data_model = None
+        self.observer = None
+        self.elevation_strategy = None
+
+    @abstractmethod
+    def set_data_model(self, data_model):
+        pass
+
+    @abstractmethod
+    def set_mode_of_elevation(self, mode_of_elevation):
+        pass
