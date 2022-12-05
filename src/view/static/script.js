9c7ca5fd67f45c8547ac108571464a79336b7722
Maanu <47236863+Letzt@users.noreply.github.com>
2022-12-05 13:15:42 -0500

    Update the UI
    Adding missing pieces in UI template

--- src/view/static/script.js
+++ src/view/static/script.js
@@ -13,12 +13,16 @@ var limitingPercent = 0;
 var shortestPathDist = 0;
 var elenaPathDistance = 0;
 
+function roundOff(N) {
+    return Math.round(N*10000)/10000;
+};
+
 function setMapMarker(type, e) {
     if (type == "source") {
-        document.getElementById("source").innerHTML = e.lngLat["lat"]+","+e.lngLat["lng"];
-        return new mapboxgl.Marker({color: "blue"}).setLngLat(e.lngLat).addTo(map);
+        document.getElementById("source").innerHTML = roundOff(e.lngLat["lat"])+","+roundOff(e.lngLat["lng"]);
+        return new mapboxgl.Marker({color: "green"}).setLngLat(e.lngLat).addTo(map);
     } else {
-        document.getElementById("destination").innerHTML = e.lngLat["lat"]+","+e.lngLat["lng"];
+        document.getElementById("destination").innerHTML = roundOff(e.lngLat["lat"])+","+roundOff(e.lngLat["lng"]);
         return new mapboxgl.Marker({color: "red"}).setLngLat(e.lngLat).addTo(map);
     }
 }
@@ -135,7 +139,7 @@ document.getElementById("submit").onclick = function(){
     limitingPercent = document.getElementById("limitingPercent").value;
 
     if (limitingPercent < 0) {
-        alert("Wrong Percentage!!!");
+        alert("Percentage of the shortest path can't be negative. Please re-enter correct value for x before submitting.");
         return;
     }
 
@@ -143,8 +147,8 @@ document.getElementById("submit").onclick = function(){
         var submitData = {
             "source_coordinates": sourceCoordinates,
             "destination_coordinates": destinationCoordinates,
-            "minimum_maximum": minimumMaximum,
-            "algo": algo,
+            "minimum_maximum": minimumMaximum.toString(),
+            "algo": algo.toString(),
             "limiting_percent": limitingPercent
         }
 
@@ -166,14 +170,14 @@ document.getElementById("submit").onclick = function(){
         manualDestinationAddress = document.getElementById("manualDestinationAddress").value.toString();
 
         if ((manualDestinationAddress.length==0) || (manualSourceAddress.length==0)) {
-            window.alert("Problem!!!!!!")
+            window.alert("Enter valid Source and Destination Addresses!")
         }
 
         var submitData = {
             "manual_source_address": manualSourceAddress,
             "manual_destination_address": manualDestinationAddress,
-            "minimum_maximum": minimumMaximum,
-            "algo": algo,
+            "minimum_maximum": minimumMaximum.toString(),
+            "algo": algo.toString(),
             "limiting_percent": limitingPercent
         }
 
@@ -191,3 +195,116 @@ document.getElementById("submit").onclick = function(){
         });
     }
 };
+
+function plotRoute(data, endpoint) {
+    if(data["bool_pop"] == 0 || data["bool_pop"] == 1) {
+        return;
+    }
+
+    if (data["bool_pop"] === -1) {
+        if (sourceMarker) {
+            sourceMarker.remove();
+        }
+        if (destMarker) {
+            destMarker.remove();
+        }
+        resetOutputs();
+        return;
+    }
+
+    map.addSource("ele_route", {
+        "type": "geojson",
+        "data": data["elev_path_route"]
+    });
+
+    map.addLayer({
+        "id": "ele_route",
+        "type": "line",
+        "source": "ele_route",
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "blue",
+            "line-width": 5,
+        }
+    });
+
+    map.addSource("shortest_route", {
+        "type": "geojson",
+        "data": data["shortest_route"]
+    });
+
+    map.addLayer({
+        "id": "shortest_route",
+        "type": "line",
+        "source": "shortest_route",
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "red",
+            "line-width": 2
+        }
+    });
+
+    calcValues(data);
+}
+
+function calcValues(data) {
+    document.getElementById("shortestPathDist").innerHTML = data["shortDist"].toFixed(4) + " meters";
+    document.getElementById("elenaPathDistance").innerHTML = data["elev_path_dist"].toFixed(4) + " meters";
+    document.getElementById("shortestPathGain").innerHTML = data["gainShort"].toFixed(4) + " meters";
+    document.getElementById("elenaPathGain").innerHTML = data["elev_path_gain"].toFixed(4) + " meters";
+}
+
+mapboxgl.accessToken = access_key;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/navigation-night-v1',
+    center: [-72.529262, 42.384803],
+    zoom: 12,
+});
+var sourceMarker, destMarker;
+marks = turf.featureCollection([]);
+
+map.on("load" , function(){
+    map.addSource('circleData', {
+        type: 'geojson',
+        data: {
+        type: 'FeatureCollection',
+        features: [],
+        },
+    });
+    map.addLayer({
+        id: 'data',
+        type: 'circle',
+        source: 'circleData',
+        paint: {
+        'circle-opacity' : 0.1,
+        'circle-radius': 300,
+        'circle-stroke-width': 2,
+        'circle-stroke-color': '#333',
+        },
+    });
+});
+
+map.on('click', function(e){
+    if(selected) {
+        lngLat = new Array(e.lngLat.lng, e.lngLat.lat);
+        if(!sourceFixed) {
+            sourceMarker = setMapMarker('source', e);
+            sourceCoordinates = JSON.stringify(e.lngLat);
+            sourceFixed = true;
+            map.flyTo({center: lngLat});
+        } else if (!destFixed){
+            destMarker = setMapMarker('destination', e);
+            destinationCoordinates = JSON.stringify(e.lngLat);
+            destFixed = true;
+            map.flyTo({center: lngLat});
+        }
+
+    }
+});
